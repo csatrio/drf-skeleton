6ed3e97f3ed65db498c10d8bd59f3c6ee2f44b39
Satrio <c.satrio@staff.gramedia.com>
2019-09-21 02:00:23 +0700

    fix cannot create superuser

--- src/appuser/models.py
+++ src/appuser/models.py
@@ -32,6 +32,8 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     class Meta:
